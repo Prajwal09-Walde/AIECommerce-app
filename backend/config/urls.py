bbf4a639992cef844835042c82791c7608f167b9
Prajwal09-Walde <pbwalde98@gmail.com>
2026-06-21 14:40:21 +0530

    Make health check endpoint async to prevent connection cancel tracebacks

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.http import JsonResponse
 
 
-def health_check(request):
+async def health_check(request):
     return JsonResponse({"status": "ok"})
 
 
